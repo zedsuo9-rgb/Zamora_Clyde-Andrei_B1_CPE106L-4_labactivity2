3db387df472f810157d7265495516dbd5b9a5072
zedsuo9-rgb <zedsuo9@gmail.com>
2026-08-11 08:32:34 +0800

    Add files via upload

---
+++ source folder/sourcecode.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a whole number: "))
+
+if number > 0:
+    print("Case 1: That is a positive number.")
+elif number < 0:
+    print("Case 2: That is a negative number.")
+else:
+    print("Case 3: You entered zero.")
